2095dd334c8e84356ce94232faab7e464fa40df0
thomas-lamiaux <thomas.lamiaux@ens-paris-saclay.fr>
2024-05-31 17:11:09 +0200

    Add a section

--- recursors_named/unit_tests.v
+++ recursors_named/unit_tests.v
@@ -21,85 +21,90 @@ Require Import postprocess_named_to_debruijn.
 Definition tmPrintb {A} (b : bool) (a : A) : TemplateMonad unit :=
   if b then tmPrint a else tmMsg "".
 
-Definition gen_rec_options (print_mdecl print_type print_term post : bool) (tm : term)
-  : TemplateMonad _ :=
-  let U := tSort sProp in
-  match tm with
-  | tInd ind0 _ =>
-    let kname := inductive_mind ind0 in
-    let pos_block := inductive_ind ind0 in
-    (* Get the mdecl definition and preprocess it *)
-    mdecl <- tmQuoteInductive (inductive_mind ind0) ;;
-    process_mdecl <- tmEval cbv (preprocessing_mind kname mdecl) ;;
-    tmPrintb print_mdecl process_mdecl ;;
-    (* Get the pos_block body under scrutiny *)
-    match nth_error process_mdecl.(ind_bodies) pos_block with
-      | Some indb =>
-        (* Compute term *)
-        named_tm_rec <- tmEval all (gen_rec_term kname process_mdecl U pos_block indb) ;;
-        tmPrintb (print_term && (negb post)) named_tm_rec ;;
-        debruijn_tm_rec <- tmEval all (named_to_debruijn 1000 named_tm_rec) ;;
-        tmPrintb (print_term && post) debruijn_tm_rec ;;
-        (* Compute type *)
-        named_ty_rec <- tmEval all (gen_rec_type kname process_mdecl U pos_block indb) ;;
-        tmPrintb (print_type && (negb post)) named_ty_rec ;;
-        debruijn_ty_rec <- tmEval all (named_to_debruijn 1000 named_ty_rec) ;;
-        tmPrintb (print_type && post) debruijn_ty_rec ;;
-        (* return *)
-        tmReturn (debruijn_tm_rec, debruijn_ty_rec)
-      | None    => tmFail "Error"
-    end
-  | _ => tmPrint tm ;; tmFail " is not an inductive"
-  end.
+Section TestFunctions.
+  Context (print_mdecl print_type print_term post : bool).
 
-Inductive mode :=
-| Debug    : mode
-| TestType : mode
-| TestTerm : mode
-| TestBoth : mode.
+  Definition gen_rec_options (tm : term)
+    : TemplateMonad _ :=
+    let U := tSort sProp in
+    match tm with
+    | tInd ind0 _ =>
+      let kname := inductive_mind ind0 in
+      let pos_block := inductive_ind ind0 in
+      (* Get the mdecl definition and preprocess it *)
+      mdecl <- tmQuoteInductive (inductive_mind ind0) ;;
+      process_mdecl <- tmEval cbv (preprocessing_mind kname mdecl) ;;
+      tmPrintb print_mdecl process_mdecl ;;
+      (* Get the pos_block body under scrutiny *)
+      match nth_error process_mdecl.(ind_bodies) pos_block with
+        | Some indb =>
+          (* Compute term *)
+          named_tm_rec <- tmEval all (gen_rec_term kname process_mdecl U pos_block indb) ;;
+          tmPrintb (print_term && (negb post)) named_tm_rec ;;
+          debruijn_tm_rec <- tmEval all (named_to_debruijn 1000 named_tm_rec) ;;
+          tmPrintb (print_term && post) debruijn_tm_rec ;;
+          (* Compute type *)
+          named_ty_rec <- tmEval all (gen_rec_type kname process_mdecl U pos_block indb) ;;
+          tmPrintb (print_type && (negb post)) named_ty_rec ;;
+          debruijn_ty_rec <- tmEval all (named_to_debruijn 1000 named_ty_rec) ;;
+          tmPrintb (print_type && post) debruijn_ty_rec ;;
+          (* return *)
+          tmReturn (debruijn_tm_rec, debruijn_ty_rec)
+        | None    => tmFail "Error"
+      end
+    | _ => tmPrint tm ;; tmFail " is not an inductive"
+    end.
 
-(* Inductive Box : Type := box (A : SProp). *)
+  Inductive mode :=
+  | Debug    : mode
+  | TestType : mode
+  | TestTerm : mode
+  | TestBoth : mode.
 
-Definition gen_rec_mode_options (m : mode) (print_mdecl print_type print_term  post : bool)
-    (tm : term) : TemplateMonad unit :=
-  t <- gen_rec_options print_mdecl print_type print_term post tm ;;
-  let tm_rec := fst t in
-  let ty_rec := snd t in
-  match m with
-  | Debug => tmMsg ""
-  | TestType =>  x <- (tmUnquote ty_rec) ;;
+  (* Inductive Box : Type := box (A : SProp).  *)
+
+  Definition gen_rec_mode_options (m : mode)
+      (tm : term) : TemplateMonad unit :=
+    t <- gen_rec_options tm ;;
+    let tm_rec := fst t in
+    let ty_rec := snd t in
+    match m with
+    | Debug => tmMsg ""
+    | TestType =>  x <- (tmUnquote ty_rec) ;;
+                    ker_ty_rec <- (tmEval all x.(my_projT2)) ;;
+                    tmPrint ker_ty_rec
+    | TestTerm =>  x <- (tmUnquote tm_rec) ;;
+                    ker_tm_rec <- (tmEval all x.(my_projT2)) ;;
+                    tmPrint ker_tm_rec
+    | TestBoth  => tmFail "bugs at the moment"
+                  (* x <- (tmUnquote ty_rec) ;;
                   ker_ty_rec <- (tmEval all x.(my_projT2)) ;;
-                  tmPrint ker_ty_rec
-  | TestTerm =>  x <- (tmUnquote tm_rec) ;;
-                  ker_tm_rec <- (tmEval all x.(my_projT2)) ;;
-                  tmPrint ker_tm_rec
-  | TestBoth  => tmFail "bugs at the moment"
-                 (* x <- (tmUnquote ty_rec) ;;
-                 ker_ty_rec <- (tmEval all x.(my_projT2)) ;;
-                 ker_tm_rec <- tmUnquoteTyped ker_ty_rec tm_rec ;;
-                 tmPrint ker_tm_rec ;;
-                 tmPrint ker_ty_rec *)
-  end.
+                  ker_tm_rec <- tmUnquoteTyped ker_ty_rec tm_rec ;;
+                  tmPrint ker_tm_rec ;;
+                  tmPrint ker_ty_rec *)
+    end.
 
-Definition print_rec_options (print_mdecl print_type print_term : bool) (q : qualid) :=
-  if print_mdecl then printInductive q else tmMsg "";;
-  if print_type then printConstantType q true else tmMsg "";;
-  if print_term then printConstantBody q true else tmMsg "".
+  Definition print_rec_options (q : qualid) :=
+    if print_mdecl then printInductive q else tmMsg "";;
+    if print_type then printConstantType q true else tmMsg "";;
+    if print_term then printConstantBody q true else tmMsg "".
+
+End TestFunctions.
 
 (* Debug preprocessing *)
 (* Definition print_rec (q : qualid) := print_rec_options false false false q.
-Definition gen_rec (tm : term) := gen_rec_mode_options Debug true false false false tm. *)
+Definition gen_rec (tm : term) := gen_rec_mode_options true false false false Debug tm. *)
 
 (* Debug Types *)
 (* Definition print_rec (q : qualid) := print_rec_options false true false q.
-Definition gen_rec (tm : term) := gen_rec_mode_options Debug false true false false tm. *)
+Definition gen_rec (tm : term) := gen_rec_mode_options false true false false Debug tm. *)
 (* Debug Terms *)
 (* Definition print_rec (q : qualid) := print_rec_options false false true q.
-Definition gen_rec (tm : term) := gen_rec_mode_options Debug false false true true tm. *)
+Definition gen_rec (tm : term) := gen_rec_mode_options false false true true Debug tm. *)
 
 (* Test Types  *)
 Definition print_rec (q : qualid) := print_rec_options false false false q.
-Definition gen_rec (tm : term) := gen_rec_mode_options TestType false false false false tm.
+Definition gen_rec (tm : term) := gen_rec_mode_options false false false false TestType tm.
 (* Test Terms *)
 (* Definition print_rec (q : qualid) := print_rec_options false false false q.
-Definition gen_rec (tm : term) := gen_rec_mode_options TestTerm false false false false tm. *)
+Definition gen_rec (tm : term) := gen_rec_mode_options false false false false TestTerm tm. *)
