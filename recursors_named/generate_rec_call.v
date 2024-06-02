6e2e3349db4a0401449da149d44e7764be285d97
thomas-lamiaux <thomas.lamiaux@ens-paris-saclay.fr>
2024-06-02 21:41:27 +0200

    refactor

---
+++ recursors_named/generate_rec_call.v
@@ -0,0 +1,80 @@
+From MetaCoq.Utils Require Import utils.
+From MetaCoq.Template Require Import All.
+
+Require Import naming.
+Require Import commons.
+
+
+Unset Guard Checking.
+
+Section GenRec.
+
+  Context (kname : kername).
+  Context (mdecl : mutual_inductive_body).
+  Context (U : term).
+  Context (E : list (kername * mutual_inductive_body * kername)).
+
+  Definition params := mdecl.(ind_params).
+  Definition nb_params := #|params|.
+  Definition relev_out_sort := relev_sort U.
+
+  MetaCoq Quote Definition qTrue := True.
+  MetaCoq Quote Definition qI := I.
+
+  Definition isSome {A} (x : option A) : bool :=
+    match x with
+    | None => false
+    | Some _ => true
+    end.
+
+  Fixpoint add_param (l : list term) (rc : list (option (term * term))) : list term * list term :=
+    match l, rc with
+    | nil, nil => (nil , nil)
+    | A::l, x :: rc =>
+      let (lty, ltm) := add_param l rc in
+      match x with
+      | None => (A :: (tLambda (mkBindAnn nAnon Relevant) A qTrue) :: lty,
+                 A :: (tLambda (mkBindAnn nAnon Relevant) A qI)    :: lty)
+      | Some (ty, tm) => (A::ty::lty, A::ty::tm::ltm)
+      end
+    | _, _ => (nil, nil)
+  end.
+
+  Fixpoint rec_pred (ty : term) {struct ty} : option (term * term) :=
+    let (hd, iargs) := decompose_app ty in
+    match hd with
+    | tInd (mkInd s pos_s) _ =>
+        if eq_constant kname s
+        then let indices := skipn nb_params iargs in
+             Some (make_pred pos_s indices,
+                  tApp (tVar (make_name "F" pos_s)) indices)
+        else match find (fun x => eq_constant s (fst (fst x))) E with
+        | Some (_, s_medcl, kparam1) =>
+             let s_nb_params := s_medcl.(ind_npars) in
+             let s_params := firstn s_nb_params iargs in
+             let s_indices := skipn s_nb_params iargs in
+             let rc := map rec_pred s_params in
+             if existsb isSome rc (* Should be rc but issue WF !!! *)
+             then let (lty, ltm) := add_param s_params rc in
+             Some (tApp (tInd (mkInd kparam1 pos_s) []) (lty ++ s_indices),
+                   tApp (tInd (mkInd kparam1 pos_s) []) (ltm ++ s_indices))
+             else None
+        | None => None
+        end
+    | _ => None
+    end.
+
+  Definition gen_rec_call (pos_arg : nat) (arg_type : term) (next_closure : term) : term * term :=
+    match rec_pred arg_type with
+    | Some (P, tmP) =>
+      ( tProd (mkBindAnn nAnon relev_out_sort)
+              (tApp P [tVar (naming_arg pos_arg)])
+              next_closure,
+        tLambda (mkBindAnn nAnon relev_out_sort)
+                (tApp P [tVar (naming_arg pos_arg)])
+                next_closure
+      )
+    | None => (next_closure, next_closure)
+    end.
+
+End GenRec.
--- recursors_named/generate_types.v
+++ recursors_named/generate_types.v
@@ -5,6 +5,7 @@ From MetaCoq.Template Require Import All.
 Require Import preliminary.
 Require Import naming.
 Require Import commons.
+Require Import generate_rec_call.
 
 (* Functions in this file:
   - Must be applied to a fully named inductive type
@@ -14,14 +15,9 @@ Require Import commons.
   Genrates :
   1. Make Type Predicate
   2. Make Type Constructor
-     2.1 Rec call
-     2.2 Type cst
   3. Make Return Type
   4. Closure Type Predicates and Constructors
 *)
-
-
-Unset Guard Checking.
 
 Section GenTypes.
 
@@ -43,72 +39,18 @@ Section GenTypes.
              (make_ind kname params pos_block indices)
              U).
 
-  (* 2. Closure constructors *)
-  (* 2.1 Compute Rec Call *)
+  (* 2. Generates type of a constructors *)
+  Definition gen_rec_call_ty pos_arg arg_type next_closure : term :=
+    fst (gen_rec_call kname mdecl U E pos_arg arg_type next_closure).
 
-  (* ATTEMPTED TO HANDLE NESTED *)
-  MetaCoq Quote Definition qTrue := True.
-
-  (* Definition mylookup *)
-
-  Definition isSome {A} (x : option A) : bool :=
-    match x with
-    | None => false
-    | Some _ => true
-    end.
-
-  Fixpoint add_param (l : list term) (rc : list (option term)) : list term :=
-    match l, rc with
-    | nil, nil => nil
-    | A::l, None :: rc => A :: (tLambda (mkBindAnn nAnon Relevant) A qTrue) :: add_param l rc
-    | A::l, Some x :: rc => A :: x :: add_param l rc
-    (* Should never happen *)
-    | nil, x :: rc => nil
-    | A::l, nil => nil
-    end.
-
-  Fixpoint rec_pred (ty : term) : option term :=
-    let (hd, iargs) := decompose_app ty in
-    match hd with
-    | tInd (mkInd s pos_s) _ =>
-        if eq_constant kname s
-        then let s_indices := skipn nb_params iargs in
-             (Some (make_pred pos_s s_indices))
-        else match find (fun x => eq_constant s (fst (fst x))) E with
-        | Some (_, s_medcl, kparam1) =>
-             let s_nb_params := s_medcl.(ind_npars) in
-             let s_params := firstn s_nb_params iargs in
-             let s_indices := skipn s_nb_params iargs in
-             let rc := map rec_pred s_params in
-             if existsb (isSome (A := term)) rc (* Should be rc but issue WF !!! *)
-             then Some (tApp (tInd (mkInd kparam1 pos_s) [])
-                       ((add_param s_params rc) ++ s_indices))
-             else None
-        | None => None
-        end
-    | _ => None
-    end.
-
-  Definition gen_rec_call' (pos_arg : nat) (arg_type : term) (next_closure : term) :  term :=
-    match rec_pred arg_type with
-    | Some P =>
-        tProd (mkBindAnn nAnon relev_out_sort)
-              (tApp P [tVar (naming_arg pos_arg)])
-              next_closure
-    | None => next_closure
-    end.
-  (* ATTEMPTED TO HANDLE NESTED *)
-
-
-  (* 2.2 Generates the type associated to j-th constructor of the i-th block *)
+  (* Generates the type associated to j-th constructor of the i-th block *)
   (* (forall x0 : t0, [P x0], ..., xn : tn, P n, P (cst A0 ... Ak t0 ... tn) -> t *)
   Definition make_type_ctor (pos_block : nat) (ctor : constructor_body)
       (pos_ctor : nat) : term :=
-    closure_args_op tProd gen_rec_call' ctor.(cstr_args)      (* forall x0 : t0, [P ... x0] *)
+    closure_args_op tProd gen_rec_call_ty ctor.(cstr_args)   (* forall x0 : t0, [P ... x0] *)
       (tApp (make_pred pos_block (ctor.(cstr_indices)))      (* P (f0 i0) ... (fn in)      *)
             [tApp (make_cst kname params pos_block pos_ctor) (* Cst A0 ... Ak              *)
                   (list_tVar naming_arg ctor.(cstr_args))]). (* x0 ... xn                  *)
-
 
   (* 3. Generation Output *)
   (* forall i0 : t0, ... il : tl, forall (x : Ind A0 ... An i0 ... il), P i0 ... il x *)
@@ -120,7 +62,7 @@ Section GenTypes.
              (tApp (make_pred pos_block (list_tVar naming_indice indices)) [tVar "x"])).
 
 
-  (* 4. Closure *)
+  (* 4. Compute closure *)
   Section Closure.
 
     Context (binder : aname -> term -> term -> term).
