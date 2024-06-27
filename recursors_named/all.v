7d18ee8a22f18ea2324a483450af549d0a5d63ad
thomas-lamiaux <thomas.lamiaux@ens-paris-saclay.fr>
2024-06-27 19:49:30 +0200

    recursors_named/all.v

---
+++ recursors_named/all.v
@@ -0,0 +1,7 @@
+Require Export naming.
+Require Export commons.
+Require Export preprocess_parameters.
+Require Export preprocess_debruijn_to_named.
+Require Export generate_rec_type.
+Require Export generate_rec_term.
+Require Export postprocess_named_to_debruijn.
