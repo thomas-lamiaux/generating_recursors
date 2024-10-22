d15e325cb7923fca79cf147a00c0b22285747436
thomas-lamiaux <thomas.lamiaux@ens-paris-saclay.fr>
2024-10-22 22:17:44 +0200

    update fd th type

--- recursors_api/CustomParam/generate_fundamental_theorem_type.v
+++ recursors_api/CustomParam/generate_fundamental_theorem_type.v
@@ -12,6 +12,7 @@ Section CustomParam.
   Context (Ep : env_param).
 
 
+(* 1. Add predicate for strpos uparams *)
 Definition name_map : (ident -> ident) -> name -> name :=
   fun f name =>
   match name with
@@ -19,12 +20,11 @@ Definition name_map : (ident -> ident) -> name -> name :=
   | nAnon => nAnon
   end.
 
-(* 1. Add predicate for strpos uparams *)
-Definition closure_uparams : (list (context_decl * bool)) -> state -> (list ident -> list ident -> list ident -> state -> term) -> term :=
+Definition closure_uparams binder : (list (context_decl * bool)) -> state -> (list ident -> list ident -> list ident -> state -> term) -> term :=
   fold_right_state_opt3
     (fun _ ' (mkdecl an db ty, b) s t =>
       (* add old_param *)
-      let* id_uparam s <- kp_tProd an ty (Some "uparams") s in
+      let* id_uparam s <- kp_binder binder an ty (Some "uparams") s in
       (* add a predicate and that it holds *)
       match b with
       | false => t [id_uparam] [] [id_uparam] s
@@ -32,11 +32,11 @@ Definition closure_uparams : (list (context_decl * bool)) -> state -> (list iden
           (* add pred *)
           let nameP := name_map (fun x => ("P" ^ x)) an.(binder_name) in
           let ty_pred := tProd (mkBindAnn nAnon Relevant) (get_one_term id_uparam s) (tSort sProp) in
-          let* id_pred s <- mk_tProd (mkBindAnn nameP Relevant) ty_pred (Some "preds") s in
+          let* id_pred s <- mk_binder binder (mkBindAnn nameP Relevant) ty_pred (Some "preds") s in
           (* add it holds *)
           let nameHP := name_map (fun x => ("HP" ^ x)) an.(binder_name) in
           let ty_pred_holds :=
-            ( let* _ s <- mk_tProd (mkBindAnn nAnon Relevant) (get_one_term id_uparam s) None s in
+            ( let* _ s <- mk_binder binder (mkBindAnn nAnon Relevant) (get_one_term id_uparam s) None s in
               (mkApp (get_one_term id_pred s) (tRel 0)))
               in
           let* _ s <- mk_tProd (mkBindAnn nameHP Relevant) ty_pred_holds (Some "preds_hold") s in
@@ -45,21 +45,34 @@ Definition closure_uparams : (list (context_decl * bool)) -> state -> (list iden
     ).
 
 
-(* 1. Generate the type *)
+  (* 2. Return type *)
+  Section mkReturnType.
 
-(* 3. Compute the custom parametricty  *)
+  Context (id_uparams : list ident).
+  Context (id_uparams_preds : list ident).
+
+  Definition make_return_type : nat -> state -> term :=
+    fun pos_indb s =>
+    let* id_nuparams s <- closure_nuparams tProd kname s in
+    let* id_indices  s <- closure_indices tProd kname pos_indb s in
+    let* id_VarMatch s <- mk_tProd (mkBindAnn (nNamed "x") (get_relevance kname pos_indb s))
+        (make_ind kname pos_indb id_uparams id_nuparams id_indices s) (Some "VarMatch") s in
+    mkApp (make_ind knamep pos_indb id_uparams_preds id_nuparams id_indices s)
+            (tRel 0).
+
+  End mkReturnType.
+
+
+(* 3. Compute the type of the fundamental theorem *)
 Definition fundamental_theorem_ty (pos_indb : nat) : term :=
-  (* add inds and its param to state *)
+  (* add inds to state *)
   let s := add_mdecl kname nb_uparams mdecl init_state in
   (* 1. add inds *)
   let ' (id_inds, s) := replace_ind kname s in
   (* 2. add uparams + extra predicate *)
-  let* id_uparams id_preds id_uparams_preds s <- closure_uparams (combine (rev (get_uparams kname s)) strpos_uparams) s in
-  let* id_nuparams s <- closure_nuparams tProd kname s in
-  let* id_indices s <- closure_indices tProd kname pos_indb s in
-  let* id_VarMatch s <- mk_tProd (mkBindAnn (nNamed "x") (get_relevance kname pos_indb s))
-    (make_ind kname pos_indb id_uparams id_nuparams id_indices s) (Some "VarMatch") s in
-  mkApp (make_ind knamep pos_indb id_uparams_preds id_nuparams id_indices s)
-         (tRel 0).
+  let* id_uparams id_preds id_uparams_preds s <-
+    closure_uparams tProd (combine (rev (get_uparams kname s)) strpos_uparams) s in
+  make_return_type id_uparams id_uparams_preds pos_indb s.
+
 
 End CustomParam.
