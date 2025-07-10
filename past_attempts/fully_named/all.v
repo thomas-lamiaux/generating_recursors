7ef90e4da84717da85785832cda18e5ea4db478a
Thomas Lamiaux <thomas.lamiaux@gmail.com>
2025-07-10 23:49:52 +0200

    remove useless files

--- past_attempts/fully_named/all.v
+++
@@ -1,7 +0,0 @@
-From RecNamed Require Export naming.
-From RecNamed Require Export commons.
-From RecNamed Require Export preprocess_parameters.
-From RecNamed Require Export preprocess_debruijn_to_named.
-From RecNamed Require Export generate_rec_type.
-From RecNamed Require Export generate_rec_term.
-From RecNamed Require Export postprocess_named_to_debruijn.
